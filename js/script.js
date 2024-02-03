8652d1142ca62e09f4f85aa6f34386a840b22430
Shubham jamliya <112676575+Shubhamjamliya@users.noreply.github.com>
2024-02-04 00:34:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ function convertSecondsToTime(seconds) {
 async function getSongs(folder) {
     currFolder = folder;
     // let a=await fetch(`http://127.0.0.1:3000/${folder}/`);
-    // let a = await fetch(`./${folder}/`);
-    let a = await fetch(`https://raw.githubusercontent.com/Shubhamjamliya/spotify-clone/main/songs`);
+    let a = await fetch(`${folder}/`);
+    // let a = await fetch(`https://raw.githubusercontent.com/Shubhamjamliya/spotify-clone/main/songs`);
     let response = await a.text();
     let div = document.createElement('div');
     div.innerHTML = response;
